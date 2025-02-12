2faee63205f239284b2cbf1f66959054f77e7e81
dingxiao <11756@DESKTOP-2USBMQO>
2025-02-12 18:33:09 +0800

    玩家通过PlayerState持有组件和Set，敌人直接持有；实现AbilitySystem接口

--- Source/Aura/Private/Player/AuraPlayerState.cpp
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -3,8 +3,19 @@
 
 #include "Player/AuraPlayerState.h"
 
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
 AAuraPlayerState::AAuraPlayerState()
 {
 	NetUpdateFrequency=100.0f;
-	
+	//玩家通过PS间接持有，以防玩家死亡后重生属性归于默认
+	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
+}
+
+UAbilitySystemComponent* AAuraPlayerState::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
 }
