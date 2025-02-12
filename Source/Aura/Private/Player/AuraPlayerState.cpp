4045cd4040ae3c624660d7c69eb5156acb75091b
dingxiao <11756@DESKTOP-2USBMQO>
2025-02-12 18:43:18 +0800

    设置网络复制模式

--- Source/Aura/Private/Player/AuraPlayerState.cpp
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -12,6 +12,8 @@ AAuraPlayerState::AAuraPlayerState()
 	//玩家通过PS间接持有，以防玩家死亡后重生属性归于默认
 	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
 	AbilitySystemComponent->SetIsReplicated(true);
+	//多人网络的玩家用Mixed 如果只是单人就是Full
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
 	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
 }
 
