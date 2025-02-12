09ee97d4720f215678d35268e1ab9f2f85756a64
dingxiao <11756@DESKTOP-2USBMQO>
2025-02-12 17:38:54 +0800

    创建PlayerState并设置网络更新频率，存放更新玩家数据

---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,10 @@
+﻿// Copyright 阿香不爱玩UE
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	NetUpdateFrequency=100.0f;
+	
+}
---
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -0,0 +1,19 @@
+﻿// Copyright 阿香不爱玩UE
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AuraPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+public:
+	AAuraPlayerState();
+	
+};
