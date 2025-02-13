a49976258e713bf37f02864d95a973a6228580f4
dingxiao <11756@DESKTOP-2USBMQO>
2025-02-13 10:41:34 +0800

    制作了一个加血道具做测试，临时展示ASC修改属性的能力

---
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -0,0 +1,53 @@
+﻿// Copyright 阿香不爱玩UE
+
+
+#include "Actor/AuraEffectActor.h"
+
+#include "AbilitySystemInterface.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "Components/SphereComponent.h"
+
+
+// Sets default values
+AAuraEffectActor::AAuraEffectActor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	SetRootComponent(Mesh);
+	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
+	Sphere->SetupAttachment(GetRootComponent());
+
+	
+	
+	
+}
+
+void AAuraEffectActor::OnComponentBeginOverlapEvent(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	//TODO: 仅做演示，临时用const cast演示，编程大忌
+	if (IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
+	{
+		const UAuraAttributeSet* AuraAttributeSet = Cast<UAuraAttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UAuraAttributeSet::StaticClass()));
+		UAuraAttributeSet* MutableAura=const_cast<UAuraAttributeSet*>(AuraAttributeSet);
+		MutableAura->SetHealth(AuraAttributeSet->GetHealth()+25.f);
+		Destroy();
+	}
+}
+
+void AAuraEffectActor::OnComponentEndOverlapEvent(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+}
+
+// Called when the game starts or when spawned
+void AAuraEffectActor::BeginPlay()
+{
+	Super::BeginPlay();
+	Sphere->OnComponentBeginOverlap.AddDynamic(this,&AAuraEffectActor::OnComponentBeginOverlapEvent);
+	Sphere->OnComponentEndOverlap.AddDynamic(this,&AAuraEffectActor::OnComponentEndOverlapEvent);
+}
+
+
+
