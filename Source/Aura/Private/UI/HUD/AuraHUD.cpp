38091b21100430c9290cee537a0b268625f3d656
dingxiao <11756@DESKTOP-2USBMQO>
2025-02-13 17:07:50 +0800

    在hud中创建主界面

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,13 @@
+﻿// Copyright 阿香不爱玩UE
+
+
+#include "UI/HUD/AuraHUD.h"
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+	UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(),OverlayWidgetClass);
+	Widget->AddToViewport();
+}
