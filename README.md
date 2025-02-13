63c8dfefc6854fae995c6ce7e7273e1406ccadbb
dingxiao <11756@DESKTOP-2USBMQO>
2025-02-13 08:25:17 +0800

    更新笔记地址并完善说明

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Assets资产自行下载:通过网盘分享的文件：Assets.zip
 链接: https://pan.baidu.com/s/19oj7ePihJEgYPcOi0PSmJQ?pwd=tfuu 提取码: tfuu 
 --来自百度网盘超级会员v6的分享
+
+笔记地址：https://blog.csdn.net/qq_30100043/category_12552698.html?spm=1001.2014.3001.5482
--- Source/Aura/Private/Player/AuraPlayerState.cpp
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -12,7 +12,7 @@ AAuraPlayerState::AAuraPlayerState()
 	//玩家通过PS间接持有，以防玩家死亡后重生属性归于默认
 	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
 	AbilitySystemComponent->SetIsReplicated(true);
-	//多人网络的玩家用Mixed 如果只是单人就是Full
+	//多人网络的玩家用Mixed 如果只是单人就是Full 该复制模式要求OwnerActor的 Owner必须是Controller PlayerState的 Owner默认是Controller
 	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
 	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
 }
