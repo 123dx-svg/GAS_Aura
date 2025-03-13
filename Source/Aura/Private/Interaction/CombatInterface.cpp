7cebf9df596692ce261c1529b8c401b6a6b82b43
dingxiao <11756@DESKTOP-2USBMQO>
2025-03-13 18:09:35 +0800

    新建战斗接口用于获取角色等级

---
+++ Source/Aura/Private/Interaction/CombatInterface.cpp
@@ -0,0 +1,11 @@
+﻿// Copyright 阿香不爱玩UE
+
+
+#include "Interaction/CombatInterface.h"
+
+
+// Add default functionality here for any ICombatInterface functions that are not pure virtual.
+int32 ICombatInterface::GetPlayerLevel()
+{
+	return 0;
+}
