b55cdbf7a6130bf64e3e82726a3a52d71ccbe1af
dingxiao <11756@DESKTOP-2USBMQO>
2025-04-27 22:29:47 +0800

    1、添加打开和关闭属性栏功能 2、通过引擎官方的AssetManager添加原生的GameplayTag

---
+++ Source/Aura/Private/AuraAssetManager.cpp
@@ -0,0 +1,19 @@
+﻿// Copyright 阿香不爱玩UE
+
+
+#include "AuraAssetManager.h"
+#include "AuraGameplayTags.h"
+
+UAuraAssetManager& UAuraAssetManager::Get()
+{
+	check(GEngine);
+	UAuraAssetManager* AuraAssetManager = Cast<UAuraAssetManager>(GEngine->AssetManager);
+	return *AuraAssetManager;
+}
+
+void UAuraAssetManager::StartInitialLoading()
+{
+	Super::StartInitialLoading();
+	FAuraGameplayTags::InitializeNativeGameplayTags();
+	
+}
---
+++ Source/Aura/Public/AuraAssetManager.h
@@ -0,0 +1,22 @@
+﻿// Copyright 阿香不爱玩UE
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetManager.h"
+#include "AuraAssetManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraAssetManager : public UAssetManager
+{
+	GENERATED_BODY()
+	public:
+		
+		static UAuraAssetManager& Get();
+
+protected:
+	virtual void StartInitialLoading() override;
+};
