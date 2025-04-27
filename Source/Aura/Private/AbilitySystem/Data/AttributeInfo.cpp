c77dd547784563aa013dcd53eaa401e7562eb478
dingxiao <11756@DESKTOP-2USBMQO>
2025-04-27 23:31:54 +0800

    利用DataAsset重新组成属性

---
+++ Source/Aura/Private/AbilitySystem/Data/AttributeInfo.cpp
@@ -0,0 +1,21 @@
+﻿// Copyright 阿香不爱玩UE
+
+
+#include "AbilitySystem/Data/AttributeInfo.h"
+
+FAuraAttributeInfo UAttributeInfo::FindAttributeInfoForTag(const FGameplayTag& AttributeTag, bool bLogNotFound) const
+{
+	for (const FAuraAttributeInfo& Info : AttributeInformation)
+	{
+		// 精确匹配
+		if (Info.AttributeTag.MatchesTagExact(AttributeTag))
+		{
+			return Info;
+		}
+	}
+	if (bLogNotFound)
+	{
+		UE_LOG(LogTemp,Error, TEXT("Can not find Info for AttributeTag[%s] on AttributeInfo[%s]"), *AttributeTag.ToString(),*GetNameSafe(this));
+	}
+	return FAuraAttributeInfo();
+}
