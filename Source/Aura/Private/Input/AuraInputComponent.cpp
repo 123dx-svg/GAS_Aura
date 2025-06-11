114afd81e010f3af2ec084b03dd301ce97387c82
dingxiao <11756@DESKTOP-2USBMQO>
2025-06-11 21:57:13 +0800

    更改自定义的增强输入类，通过模板函数绑定输入映射

---
+++ Source/Aura/Private/Input/AuraInputComponent.cpp
@@ -0,0 +1,20 @@
+﻿// Copyright 阿香不爱玩UE
+
+
+#include "Input/AuraInputComponent.h"
+
+
+// Sets default values for this component's properties
+UAuraInputComponent::UAuraInputComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+
+	// ...
+}
+
+
+
+
+
---
+++ Source/Aura/Public/Input/AuraInputComponent.h
@@ -0,0 +1,52 @@
+﻿// Copyright 阿香不爱玩UE
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraInputConfig.h"
+#include "EnhancedInputComponent.h"
+#include "AuraInputComponent.generated.h"
+
+
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class AURA_API UAuraInputComponent : public UEnhancedInputComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UAuraInputComponent();
+	template<typename UserClass,typename PressedFuncType,typename ReleaseFuncType,typename HeldFuncType>
+	void BindAbilityActions(const UAuraInputConfig* InputConfig,UserClass* Object,PressedFuncType PressFunc,ReleaseFuncType ReleaseFunc,HeldFuncType HeldFunc);
+	
+
+
+};
+
+template <typename UserClass, typename PressedFuncType, typename ReleaseFuncType, typename HeldFuncType>
+void UAuraInputComponent::BindAbilityActions(const UAuraInputConfig* InputConfig, UserClass* Object,
+	PressedFuncType PressFunc, ReleaseFuncType ReleaseFunc, HeldFuncType HeldFunc)
+{
+	check(InputConfig);
+
+	for (const FAuraInputAction& InputAction : InputConfig->AbilityInputActions)
+	{
+		if (InputAction.InputAction&&InputAction.InputTag.IsValid())
+		{
+			if (PressFunc)
+			{
+				BindAction(InputAction.InputAction, ETriggerEvent::Started, Object, PressFunc, InputAction.InputTag);
+			}
+			if (ReleaseFunc)
+			{
+				BindAction(InputAction.InputAction, ETriggerEvent::Completed, Object, ReleaseFunc, InputAction.InputTag);
+			}
+			if (HeldFunc)
+			{
+				BindAction(InputAction.InputAction, ETriggerEvent::Triggered, Object, HeldFunc, InputAction.InputTag);
+			}
+		}
+	}
+}
