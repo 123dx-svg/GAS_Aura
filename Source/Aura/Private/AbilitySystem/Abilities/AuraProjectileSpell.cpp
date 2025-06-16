82b6c8b1718469a491e8e6413a7914727dcd1b09
dingxiao <11756@DESKTOP-2USBMQO>
2025-06-16 22:14:11 +0800

    创建火球发射技能蓝图，并验证蓝图的ActivateAbility先于CPP执行

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -0,0 +1,18 @@
+﻿// Copyright 阿香不爱玩UE
+
+
+#include "AbilitySystem/Abilities/AuraProjectileSpell.h"
+
+void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	GEngine->AddOnScreenDebugMessage(
+		-1,
+		3.f,
+		FColor::Yellow,
+		FString(TEXT("ActivateAbility C++"))
+	);
+}
