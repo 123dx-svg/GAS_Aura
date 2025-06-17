003ed1c4489e85ea754815e36cdfde70826b87c7
dingxiao <11756@DESKTOP-2USBMQO>
2025-06-17 22:45:46 +0800

    创建AbilityTask，构建异步蓝图函数广播鼠标点击数据，但是存在服务端接受数据和执行函数时机无法确定问题

---
+++ Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
@@ -0,0 +1,19 @@
+﻿// Copyright 阿香不爱玩UE
+
+
+#include "AbilitySystem/AbilityTasks/TargetDataUnderMouse.h"
+
+UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility)
+{
+	UTargetDataUnderMouse* TargetData = NewAbilityTask<UTargetDataUnderMouse>(OwningAbility);
+	
+	return TargetData;
+}
+
+void UTargetDataUnderMouse::Activate()
+{
+	APlayerController* PC = Ability->GetCurrentActorInfo()->PlayerController.Get();
+	FHitResult HitResult;
+	PC->GetHitResultUnderCursor(ECC_Visibility, false, HitResult);
+	ValidData.Broadcast(HitResult.Location);
+}
---
+++ Source/Aura/Public/AbilitySystem/AbilityTasks/TargetDataUnderMouse.h
@@ -0,0 +1,34 @@
+﻿// Copyright 阿香不爱玩UE
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "TargetDataUnderMouse.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMouseTargetDataSignature, const FVector&, Data);
+
+
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UTargetDataUnderMouse : public UAbilityTask
+{
+	GENERATED_BODY()
+
+public:
+
+	//BlueprintInternalUseOnly 标记后蓝图只能搜到带类成员属性的函数否则会搜到两个版本
+
+	//获取鼠标信息
+	UFUNCTION(BlueprintCallable, Category = "Ability|Tasks", meta = (DisplayName ="TargetDataUnderMouse", HidePin = "OwningAbility", DefaultToSelf = "OwningAbility",BlueprintInternalUseOnly = "true"))
+	static UTargetDataUnderMouse* CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility);
+
+	UPROPERTY(BlueprintAssignable)
+	FMouseTargetDataSignature ValidData;
+
+private:
+	virtual void Activate() override;
+};
