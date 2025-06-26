16d8ce51ca1453482556bfaccaefaff7fa30e798
dingxiao <11756@DESKTOP-2USBMQO>
2025-06-26 22:47:07 +0800

    利用DataAsset集中管理类对象数据，JSON和CSV的使用

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,10 @@
+﻿// Copyright 阿香不爱玩UE
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
